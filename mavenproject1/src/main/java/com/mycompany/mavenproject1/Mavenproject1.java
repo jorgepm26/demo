b550a3ddaff3188190af7eea1806ccff26d865b5
jorgepm26 <143895211+jorgepm26@users.noreply.github.com>
2023-12-21 17:41:15 -0500

    Mi primer commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+import javax.swing.*;
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "áéíóú");
+    }
+}
